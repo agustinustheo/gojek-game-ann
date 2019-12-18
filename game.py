02f6a5fc4a7c52f0584186fe2ea14193bdd800fc
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2019-12-18 20:57:17 +0700

    Update: updated game mechanics and added a random obstacle spawner, obstacle spawn delay and simplified display for ANN training.

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
+import os
 import pygame
+import random
 
-pygame.init()
 
+# Define colors
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
@@ -9,53 +11,55 @@ green = (0,255,0)
 blue = (0,0,255)
 gray = (141,139,160)
 
+# Define size
 display_width=280
 display_height=449
 padding=41
+
+# Initialize game
+pygame.init()
 gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('A bit Racey')
+pygame.display.set_caption('Gojek - Pasti Ada Jalan')
 clock = pygame.time.Clock()
 
 playerImg = pygame.image.load('objects/player.png')
 player_img_size = playerImg.get_rect().size
 
-background = pygame.image.load('objects/background.png')
-obs1 = pygame.image.load('objects/obstacle1.jpg')
-obs2 = pygame.image.load('objects/obstacle2.jpg')
+# Get background
+background = pygame.image.load('game-snapshots/full_snap__bg.png')
 
-playerPos = 0
+array_of_obstacles = []
+dirs = os.listdir('objects')
+for x in dirs:
+    if str(os.path.splitext(x)[1]) == ".png" and str(os.path.splitext(x)[0]) != "warning" and str(os.path.splitext(x)[0]) != "player":
+        array_of_obstacles.append('objects/'+x)
 
 def player(x, y):
+    # Spawn player image
     gameDisplay.blit(playerImg, (x, y))
+    # Spawn player replacement
+    pygame.draw.rect(gameDisplay, red, [x, y, player_img_size[0], player_img_size[1]])
+
+def add_obstacles():
+    rand_index=random.randrange(0,len(array_of_obstacles))
+    obstacle = pygame.image.load(array_of_obstacles[rand_index])
+    return obstacle
 
 def game_loop():
+    x_change=0
+    spawn_timer=0
+    obstacle_speed=10
 
-    x = 70#(display_width * 0.45)
-    y = display_height-86#(display_height * 0.8)
+    playerPos=0
+    position_coordinates=[65, 157]
+    
+    x = position_coordinates[playerPos]
+    y = display_height-86
 
-    bgY = 0 #posisi obs
-    bgY2 = -350 #posisi obs
-
-    x_change = 0
-
+    generated_obstacles=[]
     crashed = False
     while not crashed:
         clock.tick(15)
-
-        bgY += 10  # Move both background images back
-        bgY2 += 10
-
-        if bgY > background.get_height() :  #reset obs kiri
-            bgY = -1000
-
-        if bgY2 > background.get_height() : #reset obs kanan
-            bgY2 = -320
-
-        if bgY > display_height-190 and playerPos == 0:  #obs kiri collide
-            crashed = True
-
-        if bgY2 > display_height-190 and playerPos == 1:  #obs kanan collide
-            crashed = True
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -74,13 +78,34 @@ def game_loop():
         # Create boundary
         if x+x_change <= display_width-padding-player_img_size[0] and x+x_change >= padding:
             x+=x_change
-        
-        gameDisplay.fill(white) #or gray
-        # gameDisplay.blit(background,(0,0))
-        gameDisplay.blit(obs1,(70, bgY))
-        gameDisplay.blit(obs2,(162, bgY2))
+
+        # Use background
+        gameDisplay.blit(background,(0,0))
+        # Fill with white
+        gameDisplay.fill(white)
+
+        # Spawn player
         player(x, y)
 
+        if len(generated_obstacles) <= 1 and random.randrange(0,5) == 0 and spawn_timer<=0:
+            # To delay obstacle spawn
+            spawn_timer=27
+            # Randomize position
+            pos=random.randrange(0,2)
+            # Generate obstacle and append to obstacle array
+            generated_obstacles.append([add_obstacles(), pos, -100])
+
+        for i, obstacle in enumerate(generated_obstacles):
+            obstacle[2]+=obstacle_speed
+            obstacle_img_size = obstacle[0].get_rect().size
+            # Spawn obstacle
+            gameDisplay.blit(obstacle[0], (position_coordinates[obstacle[1]], obstacle[2]))
+            # Spawn obstacle replacement
+            pygame.draw.rect(gameDisplay, black, [position_coordinates[obstacle[1]], obstacle[2], obstacle_img_size[0], obstacle_img_size[1]])
+            if obstacle[2] > display_height:
+                del generated_obstacles[i]
+
+        spawn_timer-=1
         pygame.display.update()
 
 game_loop()
