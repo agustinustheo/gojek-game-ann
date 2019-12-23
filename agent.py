21c4f511f57b831f390292272be57d555c6eed61
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2019-12-23 23:34:33 +0700

    Update: added process image function to minimize the screenshot to be processed.

--- agent.py
+++ agent.py
@@ -11,10 +11,17 @@ class GameAgent:
         x,y = 0,0
         s5_width=267
         s5_height=477
-        capture_height=360
+        capture_height=460
         box = (x, y, x+s5_width, y+capture_height)
-        im = np.array(ImageGrab.grab(box))
-        return im
+        img = np.array(ImageGrab.grab(box))
+        return img
+
+    def process_img(self, image):
+        # Resize image dimensions
+        image = cv2.resize(image, (0,0), fx = 0.075, fy = 0.0652) 
+        # Apply the canny edge detection
+        image = cv2.Canny(image, threshold1 = 100, threshold2 = 200)
+        return  image   
 
     def is_crashed(self, img):
         if np.any(img[11, 11] != 0):
@@ -36,4 +43,4 @@ class GameAgent:
 
     def get_state(self):
         img = self.screen_grab()
-        return im, is_crashed(img)
+        return self.process_img(img), self.is_crashed(img)
